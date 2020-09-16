9eb428ff2645adbe64fe9f5f6a7067db5d4d37fb
OkadaGenya <g.okada.wilf+github@gmail.com>
2020-09-16 17:13:42 +0900

    hooksを同一階層に置けなかったので、 pages配下から stateful 配下に移動

---
+++ component/stateful/top.hooks.ts
@@ -0,0 +1,96 @@
+import { useEffect, useState, useCallback } from "react"
+
+import {setToken} from "../../token"
+import firebase from '../../initialize/firebase'
+import {getAccessToken} from '../../helper/session'
+import {getScope} from '../../api/scope'
+
+type scope = {
+  acceptedScope: string | null // アクションがチェックするスコープをリスト
+  scope: string | null // トークンが承認したスコープ
+}
+
+export const useGithubLogin = () => {
+  const [isLoggedin, setIsLoggedin] = useState<boolean>(false)
+  const [scope, setScope] = useState<scope | null>(null)
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged( (user) => {
+      if(user && getAccessToken()) {
+        console.log('Login Complete!')
+        console.log(user);
+        console.log(getAccessToken());
+        setIsLoggedin(true)
+        setToken(getAccessToken())
+      }
+      else {
+        console.log('Not Login')
+        const provider = new firebase.auth.GithubAuthProvider()
+        provider.addScope('repo')
+        provider.addScope('notifications')
+        firebase.auth().signInWithPopup(provider).then((result) => {
+          console.log('auth---------', result)
+          const oauth = result.credential as firebase.auth.OAuthCredential
+
+          // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+          // PR: https://github.com/firebase/firebase-js-sdk/pull/3773#issuecomment-691599798
+          const token = oauth.accessToken
+    
+          // The signed-in user info.
+          const user = result.user;
+
+          console.warn(user, 'not use')
+    
+          setIsLoggedin(true)
+          setToken(token)
+    
+        }).catch(function(error) {
+          console.log('auth error', error)
+          // Handle Errors here.
+          console.error(error.code)
+          console.error(error.message)
+          // The email of the user's account used.
+          console.error(error.email)
+          // The firebase.auth.AuthCredential type that was used.
+          console.error(error.credential)
+        });
+      }
+    });
+  }, [])
+
+
+  useEffect(() => {
+    if (!isLoggedin) {
+      return
+    }
+    getScope().then((d: Response) => {
+      console.log('getScope', {
+        acceptedScope: d.headers.get('X-Accepted-OAuth-Scopes'),
+        scope: d.headers.get('X-OAuth-Scopes'),
+      })
+      setScope({
+        acceptedScope: d.headers.get('X-Accepted-OAuth-Scopes'),
+        scope: d.headers.get('X-OAuth-Scopes'),
+      })
+    })
+  }, [isLoggedin])
+
+  const logout = useCallback(() => {
+    if (!isLoggedin) {
+      console.warn('not logged in!')
+      return
+    }
+    firebase.auth().signOut().then(function() {
+      console.log('Sign-out successful')
+    }).catch(function(error) {
+      // An error happened.
+    });
+  }, [])
+
+
+  return {
+    isLoggedin,
+    scope,
+    logout
+  }
+}
